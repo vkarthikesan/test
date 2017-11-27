b6ebd48271e9e7ad3fafcf4a9ab7b83a1bfdd8e0
AndreaThacker <andrea.thacker@dharbor.com>
2017-11-27 11:50:12 -0600

    Auto commit for entity 3ccc9572-65b1-468f-925f-39cccd04e3bd

---
+++ 3ccc9572-65b1-468f-925f-39cccd04e3bd/src/main/java/com/dharbor/set/fusion/dynamicentityservice/model/_8b59a20914e64a48a7f84eabcfc118cb.java
@@ -0,0 +1,77 @@
+/*Digital Harbor International, Inc. makes no representations or warranties with respect to the contents or use of this manual,
+and specifically disclaims any express or implied warranties of merchantability or fitness for any particular purpose. Digital
+Harbor, Inc. reserves the right to revise this publication and to make changes to its content, at any time, without obligation to
+notify any person or entity of such revisions or changes. Digital Harbor International, Inc. makes no representations or
+warranties with respect to any Digital Harbor software, and specifically disclaims any express or implied warranties of
+merchantability or fitness for any particular purpose. There are no warranties that extend beyond the descriptions contained in
+these paragraphs. No warranty may be created or extended by sales representatives or written sales materials. Digital Harbor,
+Inc. reserves the right to make changes to any and all parts of Digital Harbor software, at any time, without any obligation to
+notify any person or entity of such changes. Digital Harbor, Inc. shall not be liable for any loss of profit or any other commercial
+damages, including but not limited to special, incidental, consequential, or other damages.
+
+Copyright © 2002-2017, Digital Harbor, Inc. All rights reserved. No part of this publication, including its interior design and\
+icons, may be reproduced, stored in a retrieval system, or transmitted in any form by any means, electronic, mechanical,
+photocopying, recording, or otherwise, without written permission of Digital Harbor.*/
+
+package com.dharbor.set.fusion.dynamicentityservice.model;
+
+import java.io.Serializable;
+import com.dharbor.set.fusion.dynamicentityservice.enums.*;
+import java.util.Date;
+import java.util.UUID;
+import java.util.List;
+import java.util.Map;
+import lombok.Data;
+import org.hibernate.validator.constraints.NotBlank;
+import org.hibernate.validator.constraints.Length;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import javax.validation.Valid;
+import org.springframework.data.rest.core.annotation.RestResource;
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+
+@Table(
+        indexes = {
+            @Index(
+                name = "findByApplicationResourceId",
+                columnList = "applicationResourceId"
+            )
+        }
+)
+@Entity
+public @Data class _8b59a20914e64a48a7f84eabcfc118cb implements BaseEntity {
+
+
+    @Id
+    @NotNull(message = "Primary key id is mandatory")
+    @Length(max = 36)
+    private String id;
+    public void setId(UUID uuidValue) {
+        this.id = (uuidValue != null) ? uuidValue.toString() : null;
+    }
+
+    @Length(max = 255)
+ 	private String personal_address_state;
+
+ 	private Integer zipcode = 0;
+
+    @Length(max = 255)
+ 	private String personal_address_apt;
+
+    @Length(max = 255)
+ 	private String personal_address_county;
+
+    @Length(max = 255)
+ 	private String personal_address_city;
+
+    @Length(max = 255)
+ 	private String personal_address_street;
+
+    @NotBlank(message = "applicationResourceId is required")
+    @Length(max = 255)
+ 	private String applicationResourceId;
+
+}
+
---
+++ 3ccc9572-65b1-468f-925f-39cccd04e3bd/src/main/java/com/dharbor/set/fusion/dynamicentityservice/repository/_3377aa29172543d1ab8faaf647fdb867Repository.java
@@ -0,0 +1,44 @@
+/*Digital Harbor International, Inc. makes no representations or warranties with respect to the contents or use of this manual,
+and specifically disclaims any express or implied warranties of merchantability or fitness for any particular purpose. Digital
+Harbor, Inc. reserves the right to revise this publication and to make changes to its content, at any time, without obligation to
+notify any person or entity of such revisions or changes. Digital Harbor International, Inc. makes no representations or
+warranties with respect to any Digital Harbor software, and specifically disclaims any express or implied warranties of
+merchantability or fitness for any particular purpose. There are no warranties that extend beyond the descriptions contained in
+these paragraphs. No warranty may be created or extended by sales representatives or written sales materials. Digital Harbor,
+Inc. reserves the right to make changes to any and all parts of Digital Harbor software, at any time, without any obligation to
+notify any person or entity of such changes. Digital Harbor, Inc. shall not be liable for any loss of profit or any other commercial
+damages, including but not limited to special, incidental, consequential, or other damages.
+
+Copyright © 2002-2017, Digital Harbor, Inc. All rights reserved. No part of this publication, including its interior design and\
+icons, may be reproduced, stored in a retrieval system, or transmitted in any form by any means, electronic, mechanical,
+photocopying, recording, or otherwise, without written permission of Digital Harbor.*/
+
+package com.dharbor.set.fusion.dynamicentityservice.repository;
+
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.data.domain.Page;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.annotation.RestResource;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.format.annotation.DateTimeFormat;
+import com.dharbor.set.fusion.dynamicentityservice.model._3377aa29172543d1ab8faaf647fdb867;
+import com.dharbor.set.fusion.dynamicentityservice.enums.*;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.*;
+
+@Api(tags = "_3377aa29172543d1ab8faaf647fdb867:")
+@RepositoryRestResource(path="3377aa29-1725-43d1-ab8f-aaf647fdb867")
+public interface _3377aa29172543d1ab8faaf647fdb867Repository extends JpaRepository<_3377aa29172543d1ab8faaf647fdb867, String>{
+    @RestResource(path="findByApplication_resource_id")
+    @Transactional
+    Page<_3377aa29172543d1ab8faaf647fdb867> findByApplicationResourceId(
+             @Param("application_resource_id") @RequestParam("application_resource_id") String application_resource_id,
+             @Param("pageable") @RequestParam("pageable") Pageable pageable
+    );
+
+}
