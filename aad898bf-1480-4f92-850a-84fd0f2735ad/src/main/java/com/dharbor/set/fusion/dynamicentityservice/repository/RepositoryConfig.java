7fcef6c28e777519e63536b21c7cd59d34528a28
Tina Ramsey <tina.ramsey@dharbor.com>
2017-11-30 09:44:36 -0800

    Auto commit for entity aad898bf-1480-4f92-850a-84fd0f2735ad

---
+++ aad898bf-1480-4f92-850a-84fd0f2735ad/src/main/java/com/dharbor/set/fusion/dynamicentityservice/repository/RepositoryConfig.java
@@ -0,0 +1,33 @@
+/*Digital Harbor International, Inc. makes no representations or warranties with respect to the contents or use of this manual,
+and specifically disclaims any express or implied warranties of merchantability or fitness for any particular purpose. Digital
+Harbor, Inc. reserves the right to revise this publication and to make changes to its content, at any time, without obligation to
+notify any person or entity of such revisions or changes. Digital Harbor International, Inc. makes no representations or
+warranties with respect to any Digital Harbor software, and specifically disclaims any express or implied warranties of
+merchantability or fitness for any particular purpose. There are no warranties that extend beyond the descriptions contained in
+these paragraphs. No warranty may be created or extended by sales representatives or written sales materials. Digital Harbor,
+Inc. reserves the right to make changes to any and all parts of Digital Harbor software, at any time, without any obligation to
+notify any person or entity of such changes. Digital Harbor, Inc. shall not be liable for any loss of profit or any other commercial
+damages, including but not limited to special, incidental, consequential, or other damages.
+
+Copyright © 2002-2017, Digital Harbor, Inc. All rights reserved. No part of this publication, including its interior design and\
+icons, may be reproduced, stored in a retrieval system, or transmitted in any form by any means, electronic, mechanical,
+photocopying, recording, or otherwise, without written permission of Digital Harbor.*/
+
+package com.dharbor.set.fusion.dynamicentityservice.repository;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurerAdapter;
+
+import org.springframework.stereotype.Repository;
+import com.dharbor.set.fusion.dynamicentityservice.model.*;
+
+@Configuration
+public class RepositoryConfig extends RepositoryRestConfigurerAdapter {
+
+    @Override
+    public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config) {
+         	config.exposeIdsFor(_a1d2f3b302f243b9891e75ad2d340758.class);
+         	config.exposeIdsFor(_25cb4265216a4c1fb04f949a15d61177.class);
+    }
+}
