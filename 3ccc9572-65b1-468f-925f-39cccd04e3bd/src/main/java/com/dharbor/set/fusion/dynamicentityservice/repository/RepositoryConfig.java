1c1cb719d01d73d7f24bef2fb3ae4564e437e4bd
Adnan Faruq <faruq@hotmail.com>
2017-12-05 16:46:10 -0500

    Auto commit for entity 3ccc9572-65b1-468f-925f-39cccd04e3bd

--- 3ccc9572-65b1-468f-925f-39cccd04e3bd/src/main/java/com/dharbor/set/fusion/dynamicentityservice/repository/RepositoryConfig.java
+++ 3ccc9572-65b1-468f-925f-39cccd04e3bd/src/main/java/com/dharbor/set/fusion/dynamicentityservice/repository/RepositoryConfig.java
@@ -30,9 +30,9 @@ public class RepositoryConfig extends RepositoryRestConfigurerAdapter {
          	config.exposeIdsFor(_98ae45aedf2e4120aab29e9b0b0a78b7.class);
          	config.exposeIdsFor(_90ae45aedf2e4120aab29e9b0b0a78b7.class);
          	config.exposeIdsFor(_bfeafbc057b64f91ab612b84696670a3.class);
-         	config.exposeIdsFor(_14f2eccfa37f43dbb86128aa7548f5f9.class);
          	config.exposeIdsFor(_89ae45aedf2e4120aab29e9b0b0a78b7.class);
          	config.exposeIdsFor(_94ae45aedf2e4120aab29e9b0b0a78b7_conviction_table.class);
+         	config.exposeIdsFor(_14f2eccfa37f43dbb86128aa7548f5f9.class);
          	config.exposeIdsFor(_94ae45aedf2e4120aab29e9b0b0a78b7_settlement_table.class);
          	config.exposeIdsFor(_93ae45aedf2e4120aab29e9b0b0a78b7.class);
          	config.exposeIdsFor(_bfeafaa457c44f91ac612b84696670a3.class);
@@ -51,8 +51,8 @@ public class RepositoryConfig extends RepositoryRestConfigurerAdapter {
          	config.exposeIdsFor(_92ae45aedf2e4120aab29e9b0b0a78b7_prof_cert_table_name_1.class);
          	config.exposeIdsFor(_f13e7e73496f4cfcab511da8736da88d_prof_cert_table_name.class);
          	config.exposeIdsFor(_ca98529941b3449daa514284866eb762.class);
-         	config.exposeIdsFor(_bfeafcc157c64f91ac612b84696670a3.class);
          	config.exposeIdsFor(_56a3b6581cc14aacbb123a4fb3d86924.class);
+         	config.exposeIdsFor(_bfeafcc157c64f91ac612b84696670a3.class);
          	config.exposeIdsFor(_56a3b6581cc14aacbb123a4fb3d86924_prof_cert_table_name_2.class);
          	config.exposeIdsFor(_f13e7e73496f4cfcab511d4403fda88d.class);
          	config.exposeIdsFor(_93ae45aedf2e4120aab29e9b0b0a78b7_legal_name_table.class);
