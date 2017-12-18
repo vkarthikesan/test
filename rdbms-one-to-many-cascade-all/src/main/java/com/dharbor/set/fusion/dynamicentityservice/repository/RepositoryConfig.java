7838d15045355e2d10603726030883877e94a060
Jenkins Server <jenkins@dh-india.com>
2017-12-18 11:53:54 +0000

    Auto commit for entity rdbms-one-to-many-cascade-all

---
+++ rdbms-one-to-many-cascade-all/src/main/java/com/dharbor/set/fusion/dynamicentityservice/repository/RepositoryConfig.java
@@ -0,0 +1,20 @@
+package com.dharbor.set.fusion.dynamicentityservice.repository;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurerAdapter;
+
+import org.springframework.stereotype.Repository;
+import com.dharbor.set.fusion.dynamicentityservice.model.*;
+
+@Configuration
+public class RepositoryConfig extends RepositoryRestConfigurerAdapter {
+
+    @Override
+    public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config) {
+         	config.exposeIdsFor(ProfilePicture.class);
+         	config.exposeIdsFor(Salary.class);
+         	config.exposeIdsFor(Employee.class);
+         	config.exposeIdsFor(Address.class);
+    }
+}
