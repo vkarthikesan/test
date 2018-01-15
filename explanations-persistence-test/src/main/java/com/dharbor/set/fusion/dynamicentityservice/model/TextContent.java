3591161ef99d6c9bbfc904b4b1a1b5ec246602fd
Ricardo Veizaga <ricardo.veizaga@dharbor.com>
2018-01-15 11:52:14 -0400

    Auto commit for entity explanations-persistence-test

--- explanations-persistence-test/src/main/java/com/dharbor/set/fusion/dynamicentityservice/model/TextContent.java
+++ explanations-persistence-test/src/main/java/com/dharbor/set/fusion/dynamicentityservice/model/TextContent.java
@@ -43,7 +43,7 @@ public @Data class TextContent implements BaseEntity {
 
     @OneToOne(
             optional = true,
-            cascade = {CascadeType.MERGE},
+            cascade = {CascadeType.MERGE,CascadeType.REFRESH},
             fetch = FetchType.EAGER
     )
     @JoinColumn(unique = true)
