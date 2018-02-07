0cfb615ec093e3e88ce250672210fe5eb4a67413
Ricardo Veizaga <ricardo.veizaga@dharbor.com>
2018-02-07 14:40:34 -0400

    Auto commit for entity chat-persistence

--- chat-persistence/src/main/java/com/dharbor/set/fusion/dynamicentityservice/model/GenericMetaDataInfo.java
+++ chat-persistence/src/main/java/com/dharbor/set/fusion/dynamicentityservice/model/GenericMetaDataInfo.java
@@ -33,7 +33,7 @@ public class GenericMetaDataInfo implements InfoContributor{
     GenericMetaDataInfo(){
         this.persistenceBuilderVersion = "2.0.0-SNAPSHOT";
         this.dynamicServiceVersion = "0.0.1-SNAPSHOT";
-        this.publishedDateOfDynamicEntityService = "2018-Feb-07 18:13:20";
+        this.publishedDateOfDynamicEntityService = "2018-Feb-07 18:40:23";
     }
 
     @Override
--- chat-persistence/src/main/java/com/dharbor/set/fusion/dynamicentityservice/repository/MessageRepository.java
+++ chat-persistence/src/main/java/com/dharbor/set/fusion/dynamicentityservice/repository/MessageRepository.java
@@ -77,12 +77,12 @@ public interface MessageRepository extends MongoRepository<Message, String>{
 
     @RestResource(path="findByConversationSeenType")
     @ApiOperation(
-        value = "findTop1ByConversationIdAndMessageTypeAndSeen", notes = "Query: {'conversationId':'?0', 'seen': '?1', 'messageType': '?2'}"
+        value = "findOneByConversationIdAndMessageTypeAndSeen", notes = "Query: {'conversationId':'?0', 'seen': '?1', 'messageType': '?2'}"
     )
     @Query(
         value = "{'conversationId':'?0', 'seen': '?1', 'messageType': '?2'}"
     )
-    Message findTop1ByConversationIdAndMessageTypeAndSeen(
+    Message findOneByConversationIdAndMessageTypeAndSeen(
              @Param("conversationId") @RequestParam("conversationId") String conversationId,
              @Param("seen") @RequestParam("seen") Boolean seen,
              @Param("messageType") @RequestParam("messageType") MessageType messageType
