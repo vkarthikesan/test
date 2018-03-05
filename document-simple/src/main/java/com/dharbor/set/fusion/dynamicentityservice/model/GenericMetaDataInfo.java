5d6bfb48aa62fae1c498c3687d0731e6d88547bc
Jenkins Server <jenkins@dh-india.com>
2018-03-05 12:42:48 +0000

    Auto commit for entity document-simple

--- document-simple/src/main/java/com/dharbor/set/fusion/dynamicentityservice/model/GenericMetaDataInfo.java
+++ document-simple/src/main/java/com/dharbor/set/fusion/dynamicentityservice/model/GenericMetaDataInfo.java
@@ -33,7 +33,7 @@ public class GenericMetaDataInfo implements InfoContributor{
     GenericMetaDataInfo(){
         this.persistenceBuilderVersion = "2.0.0-SNAPSHOT";
         this.dynamicServiceVersion = "0.0.1-SNAPSHOT";
-        this.publishedDateOfDynamicEntityService = "2018-Mar-05 12:42:24";
+        this.publishedDateOfDynamicEntityService = "2018-Mar-05 12:42:37";
     }
 
     @Override
