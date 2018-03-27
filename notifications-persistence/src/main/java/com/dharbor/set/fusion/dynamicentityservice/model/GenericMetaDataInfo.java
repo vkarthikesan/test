635781629461bf1e443aad9c73de2bd25b2e3788
Ricardo Veizaga <ricardo.veizaga@dharbor.com>
2018-03-27 16:07:54 -0400

    Auto commit for entity notifications-persistence

--- notifications-persistence/src/main/java/com/dharbor/set/fusion/dynamicentityservice/model/GenericMetaDataInfo.java
+++ notifications-persistence/src/main/java/com/dharbor/set/fusion/dynamicentityservice/model/GenericMetaDataInfo.java
@@ -33,7 +33,7 @@ public class GenericMetaDataInfo implements InfoContributor{
     GenericMetaDataInfo(){
         this.persistenceBuilderVersion = "2.0.0-SNAPSHOT";
         this.dynamicServiceVersion = "0.0.1-SNAPSHOT";
-        this.publishedDateOfDynamicEntityService = "2018-Feb-15 14:01:18";
+        this.publishedDateOfDynamicEntityService = "2018-Mar-27 20:07:28";
     }
 
     @Override
